dd501710ca0e85e6df38987be7ba7adaa5d690dd
GUEST-1001 <nawatgame@live.com>
2022-11-26 11:21:59 +0700

    WorkshopAWeek3

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Eiei');
+  res.status(200).json({
+    fullname: 'Chwanpar Kodchavong'
+  })
 });
+
+
+router.get('/bio', function(req, res, next) {
+  //res.send('Eiei');
+  res.status(200).json({
+    fullname: 'Chwanpar Kodchavong',
+    nickname: 'Bam',
+    hobby: 'play game',
+    gitusername: 'Chwanpar'
+  })
+});
+
+
 
 module.exports = router;
