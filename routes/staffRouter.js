8218e1052e50788d2fac2f0d62b96d95f054ebc1
Chwanpar <ko.chwanpar_st@tni.ac.th>
2023-02-04 10:21:28 +0700

    WorkshopA

--- routes/staffRouter.js
+++ routes/staffRouter.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const { body } = require("express-validator");
+const passportJWT = require("../middleware/passportJWT")
 
 const companyController = require('../controllers/companyController')
 
 
-router.get('/', companyController.index)
+router.get('/',[passportJWT.islogin] , companyController.index)
 router.get('/:id', companyController.show)
 router.post('/',[body('name').not().isEmpty().withMessage("กรุณาป้อนชื่อและนามสกุลด้วย"),
                 body('salary').not().isEmpty().withMessage("กรุณาใส่ข้อมูลด้วย").isNumeric().withMessage("กรุณาใส่เป็นตัวเลข")], companyController.insert)
